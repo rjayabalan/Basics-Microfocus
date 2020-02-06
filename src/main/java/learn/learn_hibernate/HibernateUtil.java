4427da0cad3a14bf03dc7060be8ea6b177f136e5
rjayabalan <ravibalajiachi.jayabalan@microfocus.com>
2020-02-06 17:03:17 +0530

    Hibernate rels

---
+++ src/main/java/learn/learn_hibernate/HibernateUtil.java
@@ -0,0 +1,34 @@
+package learn.learn_hibernate;
+
+import java.io.File;
+
+import org.hibernate.SessionFactory;
+import org.hibernate.cfg.Configuration;
+
+public class HibernateUtil {
+
+    public static final SessionFactory sessionFactory =  buildSessionFactory();
+
+    private static SessionFactory buildSessionFactory() {
+        
+        try{
+            // Create the SessionFactory from hibernate.cfg.xml
+            return new Configuration().configure(new File("src\\main\\java\\learn\\learn_hibernate\\hibernate.cfg.xml")).buildSessionFactory();
+        } catch (Throwable ex){
+
+        // Make sure you log the exception, as it might be swallowed
+        System.err.println("Initial SessionFactory creation failed." + ex);
+        throw new ExceptionInInitializerError(ex);
+        }
+    }
+
+    public static SessionFactory getSessionFactory(){
+        return sessionFactory;
+    }
+
+    public static void shutdown(){
+        getSessionFactory().close();
+    }
+    
+
+}
--- src/main/java/learn/learn_hibernate/StoreData.java
+++ src/main/java/learn/learn_hibernate/StoreData.java
@@ -1,24 +1,15 @@
 package learn.learn_hibernate;
 
-import org.hibernate.Session;    
-import org.hibernate.SessionFactory;    
-import org.hibernate.Transaction;  
-import org.hibernate.boot.Metadata;  
-import org.hibernate.boot.MetadataSources;  
-import org.hibernate.boot.registry.StandardServiceRegistry;  
-import org.hibernate.boot.registry.StandardServiceRegistryBuilder;  
+import org.hibernate.Session;     
   
-    
 public class StoreData {    
 public static void main(String[] args) {    
         
-    StandardServiceRegistry ssr = new StandardServiceRegistryBuilder().configure("hibernate.cfg.xml").build();  
-    Metadata meta = new MetadataSources(ssr).getMetadataBuilder().build();  
-  
-SessionFactory factory = meta.getSessionFactoryBuilder().build();  
-Session session = factory.openSession();  
-Transaction t = session.beginTransaction();   
+    
+    Session session = HibernateUtil.getSessionFactory().openSession();
+    session.beginTransaction();
             
+    //Adding a new Employee
     Employee e1=new Employee();  
       
     e1.setId(101);    
@@ -26,10 +17,8 @@ Transaction t = session.beginTransaction();
     e1.setLastName("Chawla");    
         
     session.save(e1);  
-    t.commit();  
-    System.out.println("successfully saved");    
-    factory.close();  
-    session.close();    
+    session.getTransaction().commit();
+    HibernateUtil.shutdown();
         
 }    
 }   
