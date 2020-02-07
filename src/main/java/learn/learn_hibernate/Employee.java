a11673d49822d4f47717585efcfd490da3511eb9
rjayabalan <ravibalajiachi.jayabalan@microfocus.com>
2020-02-07 13:56:06 +0530

    Hibernate Tuts

--- src/main/java/learn/learn_hibernate/Employee.java
+++ src/main/java/learn/learn_hibernate/Employee.java
@@ -1,33 +1,50 @@
 package learn.learn_hibernate;
 
-import javax.persistence.Entity;  
+import java.io.Serializable;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
 import javax.persistence.Id;  
-import javax.persistence.Table;  
+import javax.persistence.Table;
+import javax.persistence.UniqueConstraint;  
   
 @Entity  
-@Table(name= "emp500")   
-public class Employee {    
+@Table(name= "emp500" ,uniqueConstraints = {
+    @UniqueConstraint (columnNames = "ID")
+})   
+public class Employee implements Serializable{    
   
-@Id   
-private int id;    
-private String firstName,lastName;    
+    private static final long serialVersionUID = -1798070786993154676L;
+
+    @Id   
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "ID", unique = true, nullable = false)
+    private int id;    
+
+    @Column(name="FNAME", unique = false, nullable = false, length = 100)
+    private String firstName;
     
-public int getId() {    
-    return id;    
-}    
-public void setId(int id) {    
-    this.id = id;    
-}    
-public String getFirstName() {    
-    return firstName;    
-}    
-public void setFirstName(String firstName) {    
-    this.firstName = firstName;    
-}    
-public String getLastName() {    
-    return lastName;    
-}    
-public void setLastName(String lastName) {    
-    this.lastName = lastName;    
-}    
+    @Column(name = "LNAME", unique = false, nullable = false, length = 100)
+    private String lastName;
+
+    public int getId() {    
+        return id;    
+    }    
+    public void setId(int id) {    
+        this.id = id;    
+    }    
+    public String getFirstName() {    
+        return firstName;    
+    }    
+    public void setFirstName(String firstName) {    
+        this.firstName = firstName;    
+    }    
+    public String getLastName() {    
+        return lastName;    
+    }    
+    public void setLastName(String lastName) {    
+        this.lastName = lastName;    
+    }    
 }   
--- src/main/java/learn/learn_hibernate/StoreData.java
+++ src/main/java/learn/learn_hibernate/StoreData.java
@@ -3,22 +3,52 @@ package learn.learn_hibernate;
 import org.hibernate.Session;     
   
 public class StoreData {    
-public static void main(String[] args) {    
-        
-    
-    Session session = HibernateUtil.getSessionFactory().openSession();
-    session.beginTransaction();
+
+    private static int empid = 0; 
+    public static void main(String[] args) {    
             
-    //Adding a new Employee
-    Employee e1=new Employee();  
-      
-    e1.setId(101);    
-    e1.setFirstName("Gaurav");    
-    e1.setLastName("Chawla");    
         
-    session.save(e1);  
-    session.getTransaction().commit();
-    HibernateUtil.shutdown();
+        addData();
+
+        getData(empid);
+
+        HibernateUtil.shutdown();
         
-}    
+
+    }    
+
+    public static void addData(){
+        //Opening Session to Add
+        Session session = HibernateUtil.getSessionFactory().openSession();
+        session.beginTransaction();
+                
+        //Adding a new Employee
+        Employee e1=new Employee();  
+        
+        e1.setFirstName("Ravi");    
+        e1.setLastName("Balg");    
+            
+        session.save(e1);  
+
+        empid = e1.getId();
+        session.getTransaction().commit();
+    }
+
+    public static void getData(int empid){
+        //Otherwise called as load()
+
+        //Opening session to test load() methods
+        Session sessionone = HibernateUtil.getSessionFactory().openSession();
+        sessionone.beginTransaction();
+
+        Employee emp = new Employee();
+        sessionone.load(emp, empid);
+
+        System.out.println(emp.getFirstName() + " ~ " + emp.getLastName());
+
+        sessionone.getTransaction().commit();
+
+
+    }
+
 }   
